8024903806cb926fe78f2538bdfeacc8d139284a
wSkidX <zakiramadhan600@gmail.com>
2024-10-23 09:15:50 +0700

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
       title: 'Aplikasi Peminjaman',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        fontFamily: 'Roboto',
       ),
       home: const HomePage(),
     );
@@ -29,23 +30,37 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Aplikasi Peminjaman'),
+        elevation: 0,
       ),
       drawer: Drawer(
         child: ListView(
           padding: EdgeInsets.zero,
           children: [
-            const DrawerHeader(
+            DrawerHeader(
               decoration: BoxDecoration(
                 color: Colors.blue,
+                image: DecorationImage(
+                  image: AssetImage('assets/images/loan_background.jpg'),
+                  fit: BoxFit.cover,
+                ),
               ),
-              child: Text('Menu'),
+              child: Text(
+                'Menu',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
             ),
             ListTile(
+              leading: Icon(Icons.monetization_on),
               title: const Text('Proses Peminjaman'),
               onTap: () {
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => const ProsesPeminjamanPage()),
+                  MaterialPageRoute(
+                      builder: (context) => const ProsesPeminjamanPage()),
                 );
               },
             ),
@@ -53,7 +68,15 @@ class HomePage extends StatelessWidget {
         ),
       ),
       body: const Center(
-        child: Text('Selamat datang di Aplikasi Peminjaman'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('NAMA : ZAKI RAMADHAN'),
+            Text('NIM : 2301082020'),
+            Text('KELAS : Tk-2a'),
+            Text('KODE SOAL : B'),
+          ],
+        ),
       ),
     );
   }
